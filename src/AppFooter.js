a0e7af4f8183b1f4108c1f4e778ce674363a3a11
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2019-07-22 14:49:51 +0000

    removed ng

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -7,7 +7,7 @@ export class AppFooter extends Component {
                     <div className="p-grid">
                         <div className="p-col">
                             <img src="assets/layout/images/logo-white.svg" alt="sapphire-layout"/>
-                            <div className="layout-footer-appname">PrimeNG Premium Template</div>
+                            <div className="layout-footer-appname">PrimeReact Premium Template</div>
                         </div>
                         <div className="p-col p-col-align-right">
                             <span className="ui-icon-copyright"></span>
